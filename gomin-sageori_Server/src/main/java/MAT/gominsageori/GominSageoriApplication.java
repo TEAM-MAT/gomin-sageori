b340d19533286830cd1970007b0a1d2d6afb50ef
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-03-15 21:43:21 +0900

    templates

---
+++ gomin-sageori_Server/src/main/java/MAT/gominsageori/GominSageoriApplication.java
@@ -0,0 +1,13 @@
+package MAT.gominsageori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GominSageoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GominSageoriApplication.class, args);
+	}
+
+}
---
+++ gomin-sageori_Server/src/test/java/MAT/gominsageori/GominSageoriApplicationTests.java
@@ -0,0 +1,13 @@
+package MAT.gominsageori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GominSageoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
