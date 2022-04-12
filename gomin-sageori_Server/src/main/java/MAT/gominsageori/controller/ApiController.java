520cadf58cc950ecff61e6b036c05df2ad268a87
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-04-12 15:06:58 +0900

    응답

--- gomin-sageori_Server/src/main/java/MAT/gominsageori/controller/ApiController.java
+++ gomin-sageori_Server/src/main/java/MAT/gominsageori/controller/ApiController.java
@@ -7,23 +7,25 @@ import MAT.gominsageori.service.RestaurantService;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class ApiController {
     private RestaurantService restaurantService;
     @ResponseBody
     @GetMapping("/recommedation")
-    public ResponseEntity<String> Recommend(@RequestBody RecommandParam param){
+    public ResponseEntity<List> Recommend(@RequestBody RecommandParam param){
         List<Restaurant> restaurants = restaurantService.recommandRestaurant(param);
         //추천 알고리즘 호출 및 리턴값 받기
-        return ResponseEntity.status(200).body("hi");
+        if(restaurants.size()>0){
+            return ResponseEntity.status(200).body(restaurants);
+        }
+        else{
+            return ResponseEntity.status(500).body(restaurants);
+        }
     }
 
 }
