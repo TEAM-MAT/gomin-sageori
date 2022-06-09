bc3c19734c1ffb2dd4389e0af6404185a9641bdc
Suho Han <hansuho36eie@soongsil.ac.kr>
2022-06-10 03:00:21 +0900

    선택된 값들 userSelection에 모으기

--- gomin-sageori_front/src/UI/main/Header.js
+++ gomin-sageori_front/src/UI/main/Header.js
@@ -3,33 +3,33 @@ import { jsx, css } from "@emotion/react";
 import Menu from "./Menu";
 
 function Header() {
- const divStyle = css`
-  height: 56px;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
+  const divStyle = css`
+    height: 56px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
 
-  background-color: #fff;
- `;
+    background-color: #fff;
+  `;
 
- const titleStyle = css`
-  margin: 0 0 0 31px;
+  const titleStyle = css`
+    margin: 0 0 0 31px;
 
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 
-  cursor: default;
-  font-weight: bold;
-  font-size: 18px;
- `;
+    cursor: default;
+    font-weight: bold;
+    font-size: 18px;
+  `;
 
- return (
-  <div css={divStyle} id="Header">
-   <Menu />
-   <h1 css={titleStyle}>GO-SA</h1>
-  </div>
- );
+  return (
+    <div css={divStyle} id="Header">
+      <Menu />
+      <h1 css={titleStyle}>GO-SA</h1>
+    </div>
+  );
 }
 
 export default Header;
