ae92c5748058b479a98d5b8af3a77dc5129fd53c
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-06-22 23:35:11 +0900

    url change

--- gomin-sageori_Server/src/main/java/MAT/gominsageori/controller/MemberController.java
+++ gomin-sageori_Server/src/main/java/MAT/gominsageori/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("member")
+@RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
 
