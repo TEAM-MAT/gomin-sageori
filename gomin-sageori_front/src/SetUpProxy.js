5f723338f3e49a735ec625f0ae8479915f678aca
intersoom <sallybig@naver.com>
2022-06-27 18:53:51 +0900

    setUpProxy 주석 삭제

--- gomin-sageori_front/src/SetUpProxy.js
+++ gomin-sageori_front/src/SetUpProxy.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
-    createProxyMiddleware('/api', {  //domain
-      target: 'http://localhost:8080', //express server add
+    createProxyMiddleware('/api', {
+      target: 'http://localhost:8080',
       changeOrigin: true,
     })
   )
