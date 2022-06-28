b044daacae3d43f1d4472ae6b1e4dabef87e5e5a
intersoom <sallybig@naver.com>
2022-06-28 15:06:51 +0900

    es6로 수정

--- gomin-sageori_front/src/SetUpProxy.js
+++ gomin-sageori_front/src/SetUpProxy.js
@@ -1,11 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = (app) => {
-  app.use(
-    createProxyMiddleware("/api", {
-      //domain
-      target: "http://localhost:8080", //express server add
-      changeOrigin: true,
-    })
-  );
-};
+const SetUpProxy = (app) => {
+    app.use(
+        createProxyMiddleware("/api", {
+            //domain
+            target: "http://localhost:8080",
+            changeOrigin: true,
+        })
+    );
+}
+
+export default SetUpProxy;
