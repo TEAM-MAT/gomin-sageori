ab8b80fa344b7b157f5120f0aeb7052f24e2765a
sangcheol12 <53574961+sangcheol12@users.noreply.github.com>
2022-07-04 16:58:22 +0900

    add restaurant_image
    add restaurant_image

---
+++ gomin-sageori_Server/src/main/java/MAT/gominsageori/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package MAT.gominsageori.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static_image/**")
+                .addResourceLocations("classpath:/static_image/", "/static_image/");
+    }
+}
