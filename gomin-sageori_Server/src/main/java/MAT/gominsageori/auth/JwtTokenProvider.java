e5b92e3648ada15c4517fcd866a99652e602503c
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-07-12 22:35:37 +0900

    token provider작성중

---
+++ gomin-sageori_Server/src/main/java/MAT/gominsageori/auth/JwtTokenProvider.java
@@ -0,0 +1,47 @@
+package MAT.gominsageori.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+    private String secretKey = "tester"; //need to be changed before server start
+
+    private long validTime = 60 * 60 * 1000L;
+
+    private UserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String userId, String email) {
+        Claims claims = Jwts.claims().setSubject(userId);
+        claims.put("email",email);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + validTime))
+                .signWith(SignatureAlgorithm.ES512,secretKey)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+
+    }
+
+}
