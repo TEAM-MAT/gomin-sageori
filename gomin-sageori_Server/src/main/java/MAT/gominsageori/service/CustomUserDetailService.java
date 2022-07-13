4469a5550b399036ad0a741d5369de14ab1ab781
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-07-13 23:45:14 +0900

    Create CustomUserDetailService.java

---
+++ gomin-sageori_Server/src/main/java/MAT/gominsageori/service/CustomUserDetailService.java
@@ -0,0 +1,18 @@
+package MAT.gominsageori.service;
+
+import MAT.gominsageori.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
