69b1c19bef025da71c377404897b4528822afdf3
UnbroKen <unbroken2650@gmail.com>
2022-07-21 00:25:25 +0900

    API - ec2 서버 주소  적용

--- gomin-sageori_front/src/SetUpProxy.js
+++ gomin-sageori_front/src/SetUpProxy.js
@@ -1,10 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
-    app.use(
-        createProxyMiddleware('/api', { //domain
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-        })
-    )
+  app.use(
+    createProxyMiddleware("/api", {
+      //domain
+      target: "http://15.164.111.113:8080",
+      changeOrigin: true,
+    })
+  );
 };
