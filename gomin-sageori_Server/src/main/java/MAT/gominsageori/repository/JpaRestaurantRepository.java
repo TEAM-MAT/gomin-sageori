0ad0c720224285fbf008588120286ea1d52d1a3d
sangcheol12 <53574961+sangcheol12@users.noreply.github.com>
2022-07-21 22:16:58 +0900

    modify FranchiseFiltering
    modify FranchiseFiltering

--- gomin-sageori_Server/src/main/java/MAT/gominsageori/repository/JpaRestaurantRepository.java
+++ gomin-sageori_Server/src/main/java/MAT/gominsageori/repository/JpaRestaurantRepository.java
@@ -73,16 +73,15 @@ public class JpaRestaurantRepository implements RestaurantRepository{
 
     //특징으로부터 해당 특징들은 만족하는 메뉴들을 먼저 뽑은 뒤, 해당 메뉴들 중에 대표메뉴가 존재하는 식당을 필터링.
     @Override
-    public List<Restaurant> recommendationQuery(String location, Boolean franchise, String menuFilteringQuery){
+    public List<Restaurant> recommendationQuery(String location, String franchiseFilteringQuery, String menuFilteringQuery){
         return em.createQuery("SELECT r " +
                 "FROM Restaurant r " +
                         "WHERE " +
                         "r.bestMenu IN " +
                         "( " + menuFilteringQuery + ")" +
-                        "AND r.address.location = :location " +
-                        "AND r.franchise = :franchise", Restaurant.class)
+                        "AND r.address.location = :location" +
+                        franchiseFilteringQuery,Restaurant.class )
                 .setParameter("location",location)
-                .setParameter("franchise",franchise)
                 .getResultList();
     }
 }
