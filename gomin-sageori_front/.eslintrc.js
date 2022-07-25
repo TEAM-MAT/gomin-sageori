6b9f342d50369a79491d322dd156e6a4ca5cce5c
UnbroKen <unbroken2650@gmail.com>
2022-07-26 00:49:51 +0900

    Eslint 규칙 수정

--- gomin-sageori_front/.eslintrc.js
+++ gomin-sageori_front/.eslintrc.js
@@ -1,30 +1,6 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
-  rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/destructuring-assignment': 0,
-    'react/prop-types': 0,
-    'no-unused-expressions': 'off',
-    'jsx-a11y/no-static-element-interactions': [
-      'error',
-      {
-        handlers: [
-          'onClick',
-          'onMouseDown',
-          'onMouseUp',
-          'onKeyPress',
-          'onKeyDown',
-          'onKeyUp',
-        ],
-        allowExpressionValues: true,
-      },
-    ],
-  },
-};
+{
+  "extends": ["airbnb", "plugin:prettier/recommended"],
+  "rules": {
+    "prettier/prettier": ["error"]
+  }
+}
--- gomin-sageori_front/.prettierrc.js
+++ gomin-sageori_front/.prettierrc.js
@@ -1,9 +1,12 @@
 module.exports = {
-  singleQuote: true,
-  semi: true,
-  useTabs: false,
-  tabWidth: 2,
-  trailingComma: 'all',
   printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
   arrowParens: 'avoid',
+  proseWrap: 'never',
+  endOfLine: 'auto',
 };
